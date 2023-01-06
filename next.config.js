cb138f0800af9d7777916628970f2baaf99739b6
Alexis Lianekeo <alexis.lianekeo23@gmail.com>
2023-01-06 12:36:35 -0800

    build complete

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+
+  images: {
+    domains: ["logos-world.net", "cdn-icons-png.flaticon.com", "avatars.dicebear.com"], 
+  },
+
+ 
+  
+  
 }
